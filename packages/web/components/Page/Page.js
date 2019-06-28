acfcb50e039b54f301109f26862fc0b7eac939bd
Guilherme <gui.mochila@gmail.com>
2019-06-28 17:23:42 +0200

    Add Global CSS Styles

--- packages/web/components/Page/Page.js
+++ packages/web/components/Page/Page.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import styled, { ThemeProvider, createGlobalStyle } from 'styled-components';
 import Header from '../Header';
 import Meta from '../Meta';
@@ -24,16 +24,46 @@ const InnerPage = styled.div`
   padding: 2rem;
 `;
 
+const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px; 
+  }
+  *,*:before,*:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next';
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+`;
+
 class Page extends Component {
   render() {
     return (
-      <ThemeProvider theme={theme}>
-        <StyledPage>
-          <Meta />
-          <Header />
-          <InnerPage>{this.props.children}</InnerPage>
-        </StyledPage>
-      </ThemeProvider>
+      <Fragment>
+        <GlobalStyle />
+        <ThemeProvider theme={theme}>
+          <StyledPage>
+            <Meta />
+            <Header />
+            <InnerPage>{this.props.children}</InnerPage>
+          </StyledPage>
+        </ThemeProvider>
+      </Fragment>
     );
   }
 }
