3d498cd3d8da564b539f8c55a8f91a1ef8f264d1
Guilherme <gui.mochila@gmail.com>
2019-07-06 19:52:55 +0200

    Integrated merge-graphql-schemas with graphql-import

--- packages/server/src/types/index.js
+++ packages/server/src/types/index.js
@@ -4,15 +4,19 @@ const {
   mergeTypes,
   mergeResolvers,
 } = require('merge-graphql-schemas');
+const { importSchema } = require('graphql-import');
 
 const typesArray = fileLoader(path.join(__dirname, './**/*.graphql'), {
   recursive: true,
 });
+
+const mergedWithPrisma = typesArray.map(file => importSchema(file));
+
 const resolversArray = fileLoader(path.join(__dirname, './**/*.js'), {
   recursive: true,
 });
 
 module.exports = {
   resolvers: mergeResolvers(resolversArray, { all: true }),
-  typeDefs: mergeTypes(typesArray, { all: true }),
+  typeDefs: mergeTypes(mergedWithPrisma, { all: true }),
 };
